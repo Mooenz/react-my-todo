525a55b488f2fe05d3d5906918f70021125d5891
mooenz <joss92821@hotmail.com>
2022-01-19 22:35:27 -0500

    Cambio titulo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoList from './components/TodoList';
 const App = () => {
   return (
     <div className="container py-5">
-      <h1 className='fw-bold mb-3'>App</h1>
+      <h1 className='fw-bold mb-3'>App ToDos</h1>
       <TodoList />
     </div>
   );
