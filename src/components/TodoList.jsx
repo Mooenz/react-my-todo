73fa38b4d847851c7f247ae594006e85f43e9e10
mooenz <joss92821@hotmail.com>
2022-01-19 22:34:18 -0500

    ADD funcionalidades crud

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react';
 import Formulario from './Formulario';
+import Todo from './Todo';
 
 const TodoList = () => {
-  // const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-  // const agregarTodo = (todo) => {
-  //   setTodos((old) => [...old, todo]);
-  // };
+  const agregarTodo = (todo) => {
+    setTodos((old) => [...old, todo]);
+  };
+
+  const eliminarTodo = (id) => {
+    setTodos((old) => old.filter((item) => item.id !== id));
+  };
+
+  const editarTodo = (id) => {
+    const editTodo = todos.map((item) =>
+      item.id === id ? { ...item, estado: !item.estado } : item
+    );
+    setTodos(editTodo);
+  };
 
   return (
     <>
-      <Formulario  />
+      <Formulario agregarTodo={agregarTodo} />
+
+      <h2 className="mt-4">Lista ToDos</h2>
+      <ul className="list-group list-group-numbered">
+        {todos.map((item) => (
+          <Todo key={item.id} item={item} eliminarTodo={eliminarTodo} editarTodo={editarTodo}/>
+        ))}
+      </ul>
     </>
   );
 };
